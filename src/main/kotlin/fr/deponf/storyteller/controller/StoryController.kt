d3f096a72cd8bed6ed2784e945e7c4fa89322531
s.de-pontfarcy <s.de-pontfarcy@groupeonepoint.com>
2018-05-04 16:45:01 +0200

    [fix] fixed CORS failure by adding @CrossOrigin

--- src/main/kotlin/fr/deponf/storyteller/controller/StoryController.kt
+++ src/main/kotlin/fr/deponf/storyteller/controller/StoryController.kt
@@ -1,10 +1,14 @@
 package fr.deponf.storyteller.controller
 
+import fr.deponf.storyteller.model.Story
 import fr.deponf.storyteller.service.StoryService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin(origins = ["http://localhost:4200"])
 @RestController
 class StoryController{
 
@@ -13,6 +17,11 @@ class StoryController{
 
     @GetMapping("/story")
     fun getAllStories() = storyService.findAll()
+
+    @PostMapping("/story")
+    fun createStory(){
+        storyService.save(story = Story("Sha Do"))
+    }
 
     /*@GetMapping(value = "/story")
     fun getStory(id: String) = storyService.findById(id)*/
